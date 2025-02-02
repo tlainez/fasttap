4131e901b556988fd36150759dd9be2bedc64e3a
tlainez <toni.lainez@gmail.com>
2025-02-02 23:07:53 +0100

    Eliminada ruta duplicada

--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,6 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\HiscoreController;
 
-//Route::get('/', [HomeController::class, 'index'])->name('home');
-
 // Ruta para la página principal
 Route::get('/', [HomeController::class, 'index'])->name('home');
 
